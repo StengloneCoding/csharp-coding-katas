b6bc962c4283110dd3341aef5035ef5eaa2af62f
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-09 15:34:01 +0100

    Add FizzBuzz kata with tests

---
+++ CodingKatas.Tests/Basics/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using CodingKatas.Basics;
+
+namespace CodingKatas.Tests.Basics;
+public class FizzBuzzTests
+{
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(2, "2")]
+    [InlineData(3, "Fizz")]
+    [InlineData(5, "Buzz")]
+    [InlineData(15, "FizzBuzz")]
+    public void GetFizzBuzz_ReturnsExpectedValue(int input, string expected)
+    {
+        var fizzbuzz = new FizzBuzz();
+
+        var result = fizzbuzz.GetFizzBuzzNumber(input);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CodingKatas/Basics/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿namespace CodingKatas.Basics;
+
+public class FizzBuzz
+{
+    public string GetFizzBuzzNumber(int number)
+    {
+        string result = "";
+        if (number % 3 == 0) result += "Fizz";
+        if (number % 5 == 0) result += "Buzz";
+
+        return string.IsNullOrEmpty(result) ? number.ToString() : result;
+    }
+}
