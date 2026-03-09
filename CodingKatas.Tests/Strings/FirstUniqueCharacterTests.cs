755887523717ae7dda10590f3b01a83e263c3a23
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-09 15:34:13 +0100

    Add First Unique Character kata with tests

---
+++ CodingKatas.Tests/Strings/FirstUniqueCharacterTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingKatas.Strings;
+
+public class FirstUniqueCharacterTests
+{
+    [Theory]
+    [InlineData("Bastian", 'B')]
+    [InlineData("Anna", 'A')]
+    [InlineData("anna", '\0')]
+    [InlineData("testdtest", 'd')]
+    public void GetFirstUniqueCharacter_ReturnsExpectedCharacter(string input, char expected)
+    {
+        var firstUniqueCharacter = new FirstUniqueCharacter();
+
+        char result = firstUniqueCharacter.GetFirstUniqueCharacter(input);
+
+        Assert.Equal(result, expected);
+    }
+}
---
+++ CodingKatas/Strings/FirstUniqueCharacter.cs
@@ -0,0 +1,25 @@
+﻿namespace CodingKatas.Strings;
+public class FirstUniqueCharacter
+{
+    public char GetFirstUniqueCharacter(string text)
+    {
+        if (string.IsNullOrEmpty(text)) return '\0';
+
+        Dictionary<char, int> charCountPairs = new Dictionary<char, int>();
+        foreach (char c in text)
+        {
+            if (!charCountPairs.ContainsKey(c))
+            {
+                charCountPairs[c] = 0;
+            }
+            charCountPairs[c]++;
+
+        }
+
+        foreach (char c  in text)
+        {
+            if (charCountPairs[c] == 1) return c;
+        }
+        return '\0';
+    }
+}
