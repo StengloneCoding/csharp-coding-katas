c7b247d9f7d5bf35f495b119470cb01c4bcad128
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-09 15:34:22 +0100

    Add Anagram kata with tests

---
+++ CodingKatas.Tests/Strings/AnagramTests.cs
@@ -0,0 +1,16 @@
+﻿using CodingKatas.Strings;
+
+namespace CodingKatas.Tests.Strings;
+public class AnagramTests
+{
+    [Theory]
+    [InlineData("rail safety", "fairy tales", true)]
+    [InlineData("sorry", "not sorry", false)]
+    public void IsAnagramm_ReturnsBool(string a, string b, bool expected)
+    {
+        Anagram anagramm = new Anagram();
+        bool result = anagramm.IsAnagram(a, b);
+        Assert.Equal(result, expected);
+    }
+
+}
---
+++ CodingKatas/Strings/Anagram.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingKatas.Strings;
+public class Anagram
+{
+    public bool IsAnagram(string a, string b)
+    {
+        a = a.ToLower();
+        b = b.ToLower();
+        var arrA = a.ToCharArray();
+        var arrB = b.ToCharArray();
+        Array.Sort(arrA);
+        Array.Sort(arrB);
+        return new string(arrA) == new string(arrB);
+    }
+}
