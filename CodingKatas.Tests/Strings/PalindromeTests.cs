85eb71a0b371476879d4857a7f24aaa12929e927
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-09 15:34:08 +0100

    Add Palindrome kata with tests

---
+++ CodingKatas.Tests/Strings/PalindromeTests.cs
@@ -0,0 +1,18 @@
+﻿using CodingKatas.Strings;
+
+namespace CodingKatas.Tests.Strings;
+public class PalindromeTests
+{
+    [Theory]
+    [InlineData("", false)]
+    [InlineData("Bastian", false)]
+    [InlineData("Anna", true)]
+    public void IsPalindrome_ReturnsBool(string input, bool expected)
+    {
+        var palindrome = new Palindrome();
+
+        var result = palindrome.IsPalindrome(input);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CodingKatas/Strings/Palindrome.cs
@@ -0,0 +1,13 @@
+﻿namespace CodingKatas.Strings;
+public class Palindrome
+{
+    public bool IsPalindrome(string text)
+    {
+        if (string.IsNullOrEmpty(text)) return false;
+        
+        string reversedText = new string(text.Reverse().ToArray());
+
+        return text.Equals(reversedText, StringComparison.OrdinalIgnoreCase);
+
+    }
+}
