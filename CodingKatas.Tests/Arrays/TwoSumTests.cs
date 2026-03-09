3cb7a43b0a9b68fa3a39f8d94184354a6b828762
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-09 15:34:27 +0100

    Add Two Sum kata with tests

---
+++ CodingKatas.Tests/Arrays/TwoSumTests.cs
@@ -0,0 +1,17 @@
+﻿using CodingKatas.Arrays;
+
+namespace CodingKatas.Tests.Arrays;
+public class TwoSumTests
+{
+    [Theory]
+    [InlineData(new int[] { 2, 7, 11, 15 }, 9, 0, 1)]
+    public void GetTwoSumNumbers_ReturnsArrayWithIndexes(int[] input, int target, int expectedOne, int expectedTwo)
+    {
+        var twoSum = new TwoSum();
+
+        int[] result = twoSum.GetTwoSumNumbers(input, target);
+
+        Assert.Contains(expectedOne, result);
+        Assert.Contains(expectedTwo, result);
+    }
+}
---
+++ CodingKatas/Arrays/TwoSum.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingKatas.Arrays;
+public class TwoSum
+{
+    public int[] GetTwoSumNumbers(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i+1; j < numbers.Length; j++)
+            {
+                    if (numbers[i] + numbers[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+            }
+        }
+        return Array.Empty<int>();
+    }
+}
