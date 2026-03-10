0a847c9a9ee41ff4eb665d77d58d4f861eb77ab6
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-10 09:02:01 +0100

    Add inline test cases for Two Sum kata

--- CodingKatas.Tests/Arrays/TwoSumTests.cs
+++ CodingKatas.Tests/Arrays/TwoSumTests.cs
@@ -5,6 +5,11 @@ public class TwoSumTests
 {
     [Theory]
     [InlineData(new int[] { 2, 7, 11, 15 }, 9, 0, 1)]
+    [InlineData(new int[] { 0, 4, 10, 5 }, 15, 2, 3)]
+    [InlineData(new int[] { -3, 4, 3, 90 }, 0, 0, 2)]
+    [InlineData(new int[] { 0, 4, 3, 0 }, 0, 0, 3)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, 6, 0, 4)]
+
     public void GetTwoSumNumbers_ReturnsArrayWithIndexes(int[] input, int target, int expectedOne, int expectedTwo)
     {
         var twoSum = new TwoSum();
