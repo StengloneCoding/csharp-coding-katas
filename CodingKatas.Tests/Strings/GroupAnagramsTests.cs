c6da99a02fd0a6df8c4b1d9a129ca356a1c7263e
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-12 09:06:15 +0100

    Add Group Anagrams kata with tests

---
+++ CodingKatas.Tests/Strings/GroupAnagramsTests.cs
@@ -0,0 +1,17 @@
+﻿using CodingKatas.Strings;
+
+namespace CodingKatas.Tests.Strings;
+
+public class GroupAnagramsTests
+{
+    [Fact]
+    public void GetGroupAnagrams_ReturnsCorrectGroups()
+    {
+        var input = new[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+        var groupAnagrams = new GroupAnagrams();
+        var result = groupAnagrams.GetGroupAnagrams(input);
+
+        Assert.Equal(3, result.Count);
+    }
+}
---
+++ CodingKatas/Strings/GroupAnagrams.cs
@@ -0,0 +1,21 @@
+﻿namespace CodingKatas.Strings;
+public class GroupAnagrams
+{
+    public IList<IList<string>> GetGroupAnagrams(string[] words)
+    {
+        Dictionary<string, List<string>> sortedStrings = [];
+        foreach (var word in words)   
+        {
+            char[] chars = word.ToCharArray();
+            Array.Sort(chars);
+            string currentSortedWord = new(chars);
+            if (!sortedStrings.ContainsKey(currentSortedWord))
+            {
+                sortedStrings.Add(currentSortedWord, []);
+            }
+                sortedStrings[currentSortedWord].Add(word);
+
+        }
+        return [.. sortedStrings.Values];
+    }
+}
