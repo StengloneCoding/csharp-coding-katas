2103b1538ec7049e3ff66c1aedbff10469c3c74d
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-12 09:06:38 +0100

    Add LongestSubstringWithoutRepeatingCharacters kata with tests

---
+++ CodingKatas.Tests/Strings/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,21 @@
+﻿using CodingKatas.Strings;
+
+namespace CodingKatas.Tests.Strings;
+
+public class LongestSubstringWithoutRepeatingCharactersTests
+{
+    [Theory]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("bbbbb", 1)]
+    [InlineData("pwwkew", 3)]
+    [InlineData("", 0)]
+    [InlineData("abcdef", 6)]
+    public void GetLengthOfLongestSubstring_ReturnsExpectedResult(string input, int expected)
+    {
+        LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new();
+
+        var result = longestSubstringWithoutRepeatingCharacters.GetLengthOfLongestSubstring(input);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CodingKatas/Strings/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,25 @@
+﻿namespace CodingKatas.Strings;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    public int GetLengthOfLongestSubstring(string input)
+    {
+        HashSet<char> window = [];
+        int left = 0;
+        int longestSubstring = 0;
+
+        for (int right = 0; right < input.Length; right++)
+        {
+            while (window.Contains(input[right]))
+            {
+                window.Remove(input[left]);
+                left++;
+            }
+
+            window.Add(input[right]);
+            longestSubstring = Math.Max(longestSubstring, right - left + 1);
+        }
+
+        return longestSubstring;
+    }
+}
