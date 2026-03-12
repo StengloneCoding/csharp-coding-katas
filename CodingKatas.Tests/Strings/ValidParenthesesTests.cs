807a683b92ecf186b898cf52ac413237c53cae71
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-12 09:06:55 +0100

    Add Valid Parantheses kata with tests

---
+++ CodingKatas.Tests/Strings/ValidParenthesesTests.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingKatas.Strings;
+
+public class ValidParenthesesTests
+{
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    [InlineData("([)]", false)]
+    [InlineData("{[]}", true)]
+    public void IsValid_ReturnsExpectedResult(string input, bool expected)
+    {
+        ValidParentheses validParantheses = new();
+
+        bool result = validParantheses.IsValid(input);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CodingKatas/Strings/ValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿namespace CodingKatas.Strings;
+public class ValidParentheses
+{
+    private static readonly Dictionary<char, char> ParenthesesPairs = new()
+    {
+        { ')', '(' },
+        { ']', '[' },
+        { '}', '{' }
+    };
+
+    public bool IsValid(string input)
+    {
+        Stack<char> stack = new();
+        foreach (char c in input)
+        {
+            if (!ParenthesesPairs.ContainsKey(c))
+            {
+                stack.Push(c);
+            }
+           else
+            {
+                if (stack.Count == 0) return false;
+                else if (stack.Peek() == ParenthesesPairs[c])
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+        }
+        return stack.Count == 0;
+    }
+}
