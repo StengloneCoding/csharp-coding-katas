0a6eda55663735303e334d94e61ba861c6018a6f
Bastian Stenglein <bastian.stenglein@telefonica.com>
2026-03-12 09:05:40 +0100

    Add TopKFrequent kata with tests

---
+++ CodingKatas.Tests/Arrays/TopKFrequentTests.cs
@@ -0,0 +1,19 @@
+﻿using CodingKatas.Arrays;
+
+namespace CodingKatas.Tests.Arrays;
+public class TopKFrequentTests
+{
+    [Theory]
+    [InlineData(new int[] { 1 }, 1)]
+    [InlineData(new int[] { 1, 1, 1 }, 1)]
+    [InlineData(new int[] { 1, 2, 3 }, 1)]
+    [InlineData(new int[] { 1, 1, 2, 2, 3 }, 2)]
+    public void GetTopKFrequent_ReturnsCorrectLength(int[] input, int k)
+    {
+        TopKFrequent topKFrequent = new();
+
+        int[] result = topKFrequent.GetTopKFrequent(input, k);
+
+        Assert.Equal(k, result.Length);
+    }
+}
---
+++ CodingKatas/Arrays/TopKFrequent.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace CodingKatas.Arrays;
+public class TopKFrequent
+{
+    public int[] GetTopKFrequent(int[] numbers, int k)
+    {
+        Dictionary<int, int> countedNumbers = [];
+        foreach (int number in numbers)
+        {
+            countedNumbers[number] = countedNumbers.GetValueOrDefault(number) + 1;
+        }
+        return countedNumbers.OrderByDescending(x => x.Key).Take(k).Select(x => x.Value).ToArray();
+    }
+}
